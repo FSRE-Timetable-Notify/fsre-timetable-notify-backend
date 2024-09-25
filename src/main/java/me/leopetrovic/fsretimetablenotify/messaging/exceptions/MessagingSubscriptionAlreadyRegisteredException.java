fe5adbeb12ce1569b5d044ff6ef2d64a17c56b94
mapokapo <leopetrovic11@gmail.com>
2024-09-25 21:14:46 +0200

    Fix typo

--- src/main/java/me/leopetrovic/fsretimetablenotify/messaging/exceptions/MessagingSubscriptionAlreadyRegisteredException.java
+++ src/main/java/me/leopetrovic/fsretimetablenotify/messaging/exceptions/MessagingSubscriptionAlreadyRegisteredException.java
@@ -2,14 +2,14 @@ package me.leopetrovic.fsretimetablenotify.messaging.exceptions;
 
 public class MessagingSubscriptionAlreadyRegisteredException extends RuntimeException {
 	public MessagingSubscriptionAlreadyRegisteredException(String email, String fcmToken, Long studyProgramId) {
-		super("Messaging subscription with credentails: ()" + email + ", " + fcmToken
+		super("Messaging subscription with credentails: (" + email + ", " + fcmToken
 				+ ") was already registered for study program with ID "
 				+ studyProgramId);
 	}
 
 	public MessagingSubscriptionAlreadyRegisteredException(String email, String fcmToken, Long studyProgramId,
 			Throwable cause) {
-		super("Messaging subscription with credentails: ()" + email + ", " + fcmToken
+		super("Messaging subscription with credentails: (" + email + ", " + fcmToken
 				+ ") was already registered for study program with ID "
 				+ studyProgramId, cause);
 	}
